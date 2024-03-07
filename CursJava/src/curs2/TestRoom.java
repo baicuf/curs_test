acd6d153e6a9381211c528d420067baac748a5d9
Florin Baicu <Florin.Baicu@kyndryl.com>
2024-03-07 11:27:08 +0200

    curs 2 final

---
+++ CursJava/src/curs2/TestRoom.java
@@ -0,0 +1,13 @@
+package curs2;
+
+public class TestRoom {
+
+	public static void main(String[] args) {
+		
+		Room baie = new Room(4, 2);
+		System.out.println("Perimetru baie este : " + baie.calculPerimetru());
+		System.out.println("Arie baie este : " + baie.calculArie());
+
+	}
+
+}
