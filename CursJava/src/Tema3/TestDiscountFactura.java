8ad8f461e8aafe0b551cfbea837bd71a067f1960
baicuf <florin.baicu@gmail.com>
2024-03-16 07:55:29 +0200

    Tema 3

---
+++ CursJava/src/Tema3/TestDiscountFactura.java
@@ -0,0 +1,10 @@
+package Tema3;
+
+public class TestDiscountFactura {
+
+	public static void main(String[] args) {
+		DiscountFactura obj = new DiscountFactura();
+		obj.calculateDiscounted();
+	}
+
+}
